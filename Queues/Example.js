3c5ef2de85be444982601d6e8effe8f314474431
maugarciav <mau.villanueva2002@gmail.com>
2024-01-03 12:29:43 -0600

    PriorityQueue: implementation and use

--- Queues/Example.js
+++ Queues/Example.js
@@ -1,5 +1,8 @@
 const Queue = require("./queue");
+const PriorityQueue = require("./PriorityQueue");
 
+
+// ----------------- Queue ----------------- //
 const q = new Queue();
 
 q.enqueue(1)
@@ -12,3 +15,19 @@ q.dequeue()
 console.log(q.front())
 
 q.print()
+
+
+// ----------- Priority Queue ------------ //
+
+const pq = new PriorityQueue();
+
+pq.enqueue(['Mau Garcia' , 5])
+pq.enqueue(['Cee dee Lamb' , 88])
+pq.enqueue(['Dak' , 4])
+
+pq.printCollection()
+
+pq.dequeue()
+console.log(pq.front())
+
+pq.printCollection()
