4d1d3bd3e0a1636d6ecac5191b65a9185ba477fe
maugarciav <mau.villanueva2002@gmail.com>
2024-01-05 22:28:41 -0600

    trie: implementation and use

---
+++ Trie/Example.js
@@ -0,0 +1,17 @@
+const Trie = require('./trie')
+
+const myTrie = new Trie()
+
+myTrie.add('ball')
+myTrie.add('bat')
+myTrie.add('doll')
+myTrie.add('dork')
+myTrie.add('dorm')
+myTrie.add('send')
+myTrie.add('sense')
+
+console.log(myTrie.isWord('doll'))
+console.log(myTrie.isWord('dor'))
+console.log(myTrie.isWord('dorf'))
+console.log(myTrie.print())
+
