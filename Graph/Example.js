ae17ba399600e84bf46dff6c9ad0f678b7ea769f
maugarciav <mau.villanueva2002@gmail.com>
2024-01-06 12:20:30 -0600

    graph: explanantion, implmentation && use

---
+++ Graph/Example.js
@@ -0,0 +1,13 @@
+const bfs = require('./graphsBFS.js')
+
+
+// Adejacency Matrix 
+var exBFSGraph = [
+  [0, 1, 1, 1, 0],
+  [0, 0, 1, 0, 0],
+  [1, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0],
+  [0, 1, 0, 0, 0]
+];
+
+console.log(bfs(exBFSGraph, 1));
